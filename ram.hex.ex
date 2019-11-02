b539c4429bc6d747a585363c6b5aa56b1d20aafa
Joseph Shaker <jshakes213@gmail.com>
2019-11-01 23:30:01 -0600

    added better comments to code

--- ram.hex.ex
+++ ram.hex.ex
@@ -1,15 +1,15 @@
 // Program to calculate fibonacci sequence and stop at first value over 255
-1d    // 0x0 LDA d
+1d    // 0x0 LDA d  grab X from RAM
 2e    // 0x1 ADD e  A_Reg = X+Y
 aa    // 0x2 JIC a  If carry, stop and DONT output
-e0    // 0x3 OUT
-7f    // 0x4 STA f  Store Z
+e0    // 0x3 OUT    ..
+7f    // 0x4 STA f  Store Z=X+7
 1d    // 0x5 LDA d  Y=X
-7e    // 0x6 STA e
+7e    // 0x6 STA e  ..
 1f    // 0x7 LDA f  X=Z
-7d    // 0x8 STA d
-80    // 0x9 JMP 0
-ff    // 0xa HLT
+7d    // 0x8 STA d  ..
+80    // 0x9 JMP 0  Loop
+ff    // 0xa HLT    done
 00    // 0xb NOP
 00    // 0xc NOP
 01    // 0xd "X"
