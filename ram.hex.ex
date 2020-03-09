40294a5bd6161c105f102c12e6371a9efee812ad
Joseph Shaker <jshakes213@gmail.com>
2020-03-08 18:47:22 -0600

    change program to no longer need to initialize X Y Z

--- ram.hex.ex
+++ ram.hex.ex
@@ -1,19 +1,21 @@
 // Program to calculate fibonacci sequence and stop at first value over 255
-// Unfortunately, this program cannot simply be reset, because I rely on the initialized values of
-// X and Y. There is no store immediate command. (Maybe I can come up with something more clever)
-1d    // addr:0x0 | LDA d  | Assing A_Reg = X
-2e    // addr:0x1 | ADD e  | Assign A_Reg = X+Y
-aa    // addr:0x2 | JIC a  | If carry, stop and DONT output
-e0    // addr:0x3 | OUT    | Display output Z
-7f    // addr:0x4 | STA f  | Store Z=X+Y
-1d    // addr:0x5 | LDA d  | Store Y=X
-7e    // addr:0x6 | STA e  | finish storing Y=X
-1f    // addr:0x7 | LDA f  | Store X=Z
-7d    // addr:0x8 | STA d  | finish storing X=Z
-80    // addr:0x9 | JMP 0  | Loop to calculate next number
-ff    // addr:0xa | HLT    | done
-00    // addr:0xb | NOP    | unused
-00    // addr:0xc | NOP    | unused
-01    // addr:0xd | "X"    | initialize X to 1
-00    // addr:0xe | "Y"    | initialize Y to 0
-00    // addr:0xf | "Z"    | doesnt need to be anything, Z is written to before it is read
+
+// INIT
+40    // addr:0x0 | LDI 0  | initialize Y to 0
+7e    // addr:0x1 | STA e  | continue initializing Y to 0
+41    // addr:0x2 | LDI 1  | initialize X to 1
+// LOOP
+7d    // addr:0x3 | STA d  | Initialize X(first time) / Store X
+2e    // addr:0x4 | ADD e  | Assign A_Reg = X+Y
+ac    // addr:0x5 | JIC c  | If carry, stop and DONT output
+e0    // addr:0x6 | OUT    | Display output Z
+7f    // addr:0x7 | STA f  | Store Z=X+Y
+1d    // addr:0x8 | LDA d  | Store Y=X
+7e    // addr:0x9 | STA e  | finish storing Y=X
+1f    // addr:0xa | LDA f  | Load X=Z
+83    // addr:0xb | JMP 3  | Loop to calculate next number
+ff    // addr:0xc | HLT    | done
+// VARIABLES
+aa    // addr:0xd | "X"    | These can default to anything as they are initialized before being read from
+bb    // addr:0xe | "Y"    | You can therefore simple reset the program counter and instruction counter
+cc    // addr:0xf | "Z"    | Without reporgramming these to reset the program
