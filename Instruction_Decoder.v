8f5ce7bbdfd6fc3343bc6a9fdab9c9f3625df731
jshaker000 <jshakes213@gmail.com>
2022-10-29 12:19:33 -0600

    tweaks

--- Instruction_Decoder.v
+++ Instruction_Decoder.v
@@ -17,7 +17,11 @@ module Instruction_Decoder (
 
   `include "instructions.vi"
 
-  localparam STEP_WIDTH         = $clog2(INSTRUCTION_STEPS);
+  localparam STEP_WIDTH = $clog2(INSTRUCTION_STEPS);
+
+  localparam                    [0:0] X_NOT_ZERO_FOR_SHOULD_NEVER_REACH = 1'b0;
+  localparam [CONTROL_WORD_WIDTH-1:0] SHOULD_NEVER_REACH                = {CONTROL_WORD_WIDTH{X_NOT_ZERO_FOR_SHOULD_NEVER_REACH ? 1'bx : 1'b0}};
+  localparam [CONTROL_WORD_WIDTH-1:0] ZERO_CW                           = {CONTROL_WORD_WIDTH{1'b0}};
 
   input wire     [INSTRUCTION_WIDTH-1:0] i_instruction;
   input wire            [STEP_WIDTH-1:0] i_step;
@@ -26,58 +30,74 @@ module Instruction_Decoder (
   input wire                             i_carry;
   output wire   [CONTROL_WORD_WIDTH-1:0] o_control_word;
 
-  assign o_control_word = // fetch, put prgm cntr in mem addr, fetch instruction, advance PC. All instructions start like this
+ 
+  assign o_control_word =
+                 // Fetch, put prgm cntr in mem addr, fetch instruction, advance PC. All instructions start like this
+                 // Also note, all instructions must end in a c_ADV to advance to the next instruction
                  i_step == 'h0 ? c_MI | c_CO        :
                  i_step == 'h1 ? c_RO | c_II | c_CE :
-                // i_instruction == 4'h00 ? // unimplemented - defaults at bottom to NOP
-                   i_instruction == 4'h01 ? // LDA - put data in RAM addr in A
-                     i_step == 'h2      ? c_IO | c_MI        :
-                     i_step == 'h3      ? c_RO | c_AI        :
-                     c_ADV :
-                   i_instruction == 4'h02 ? // ADD - add data from RAM addr to A
-                     i_step == 'h2      ? c_IO | c_MI        :
-                     i_step == 'h3      ? c_RO | c_BI        :
-                     i_step == 'h4      ? c_EO | c_AI | c_EL :
-                     c_ADV :
-                   i_instruction == 4'h03 ? // SUBTRACT - subtract data from RAM addr to A
+                   // i_instruction == 4'h00 ? // unimplemented - defaults at bottom to NOP
+                   // LDA - put data in RAM addr in A
+                   i_instruction == 4'h01 ?
+                     i_step == 'h2      ? c_IO | c_MI         :
+                     i_step == 'h3      ? c_RO | c_AI | c_ADV :
+                     SHOULD_NEVER_REACH:
+                   // ADD - add data from RAM addr to A, storing into A. Clobbers B
+                   i_instruction == 4'h02 ?
                      i_step == 'h2      ? c_IO | c_MI        :
                      i_step == 'h3      ? c_RO | c_BI        :
-                     i_step == 'h4      ? c_EO | c_SU | c_AI | c_EL :
-                     c_ADV :
-                   i_instruction == 4'h04 ? // LDI - load an immediate 4 bit value to A
-                     i_step == 'h2      ? c_IO | c_AI        :
-                     c_ADV :
-                   i_instruction == 4'h05 ? // ADDI - add an immediate 4 bit value to A
+                     i_step == 'h4      ? c_EO | c_AI | c_EL | c_ADV :
+                     SHOULD_NEVER_REACH:
+                   // SUBTRACT - subtract data from RAM addr to A, storing into A. Clobbers B
+                   i_instruction == 4'h03 ?
+                     i_step == 'h2      ? c_IO | c_MI        :
+                     i_step == 'h3      ? c_RO | c_BI        :
+                     i_step == 'h4      ? c_EO | c_SU | c_AI | c_EL  | c_ADV :
+                     SHOULD_NEVER_REACH:
+                   // LDI - load an immediate 4 bit value to A
+                   i_instruction == 4'h04 ?
+                     i_step == 'h2      ? c_IO | c_AI | c_ADV :
+                     SHOULD_NEVER_REACH:
+                   // ADDI - add an immediate 4 bit value to A, storing into A. Clobbers B
+                   i_instruction == 4'h05 ?
                      i_step == 'h2      ? c_IO | c_BI        :
-                     i_step == 'h3      ? c_EO | c_AI | c_EL :
-                     c_ADV :
-                   i_instruction == 4'h06 ? // SUBTRACTI - subtract an immediate 4 bit value from A
+                     i_step == 'h3      ? c_EO | c_AI | c_EL | c_ADV :
+                     SHOULD_NEVER_REACH:
+                   // SUBTRACTI - subtract an immediate 4 bit value to A, storing into A. Clobbers B
+                   i_instruction == 4'h06 ?
                      i_step == 'h2      ? c_IO | c_BI        :
-                     i_step == 'h3      ? c_EO | c_SU | c_AI | c_EL :
-                     c_ADV :
-                   i_instruction == 4'h07 ? // STA - store A in RAM
+                     i_step == 'h3      ? c_EO | c_SU | c_AI | c_EL | c_ADV :
+                     SHOULD_NEVER_REACH:
+                   // STA - store A in RAM addr
+                   i_instruction == 4'h07 ?
                      i_step == 'h2      ? c_IO | c_MI :
-                     i_step == 'h3      ? c_AO | c_RI :
-                     c_ADV :
-                   i_instruction == 4'h08 ? // JMP - jump to ADDR
-                     i_step == 'h2      ? c_IO | c_J  :
-                     c_ADV :
-                   i_instruction == 4'h09 ? // JIZ - jump to ADDR if last ALU op was 0
-                     i_step == 'h2      ? i_zero  ? (c_IO | c_J) : c_ADV :
-                     c_ADV :
-                   i_instruction == 4'h0a ? // JIC - jump to ADDR if last ALU op carried
-                     i_step == 'h2      ? i_carry ? (c_IO | c_J) : c_ADV :
-                     c_ADV :
+                     i_step == 'h3      ? c_AO | c_RI | c_ADV :
+                     SHOULD_NEVER_REACH:
+                   // JMP - jump to addr
+                   i_instruction == 4'h08 ?
+                     i_step == 'h2      ? c_IO | c_J  | c_ADV :
+                     SHOULD_NEVER_REACH:
+                   // JIZ - jump to addr if last ALU op was 0
+                   i_instruction == 4'h09 ?
+                     i_step == 'h2      ? (i_zero  ? (c_IO | c_J) : ZERO_CW) | c_ADV :
+                     SHOULD_NEVER_REACH :
+                   // JIC - jump to addr if last ALU op had a carry
+                   i_instruction == 4'h0a ?
+                     i_step == 'h2      ? (i_carry ? (c_IO | c_J) : ZERO_CW) | c_ADV :
+                     SHOULD_NEVER_REACH :
+                   // JIO - jump to ADDR if last ALU op was odd
                    i_instruction == 4'h0b ? // JIO - jump to ADDR if last ALU op was odd
-                     i_step == 'h2      ? i_odd   ? (c_IO | c_J) : c_ADV :
-                     c_ADV :
-                 // i_instruction == 4'h0c ? // unimplement - defaults at bottom to NOP
-                 // i_instruction == 4'h0d ? // unimplement - defaults at bottom to NOP
-                   i_instruction == 4'h0e ? // OUT - copy Areg to out reg
-                     i_step == 'h2      ? c_AO | c_OI :
-                     c_ADV :
-                   i_instruction == 4'h0f ? // HALT PROGRAM
+                     i_step == 'h2      ? (i_odd   ? (c_IO | c_J) : ZERO_CW) | c_ADV :
+                     SHOULD_NEVER_REACH :
+                   // i_instruction == 4'h0c ? // unimplement - defaults at bottom to NOP
+                   // i_instruction == 4'h0d ? // unimplement - defaults at bottom to NOP
+                   // OUT - copy Areg to out reg
+                   i_instruction == 4'h0e ?
+                     i_step == 'h2      ? c_AO | c_OI | c_ADV :
+                     SHOULD_NEVER_REACH :
+                   // HALT PROGRAM
+                   i_instruction == 4'h0f ?
                      c_HLT :
-                   c_ADV;                 // NOP - do nothing and just advance counter
-
+                   // NOP - do nothing and just advance counter
+                  i_step == 'h2 ? c_ADV : SHOULD_NEVER_REACH;
 endmodule
